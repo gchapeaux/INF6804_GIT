844d42f4432cb842fde3244614d6df29bb455445
Grégoire Chapeaux <gchapeaux@gmail.com>
2021-03-03 22:22:30 -0500

    Some stuffs

--- TP2_Gr7/scripts/bg_sub.py
+++ TP2_Gr7/scripts/bg_sub.py
@@ -3,16 +3,13 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
-# PATH TO THE THE VIDEO FRAMES
-videopath = 'data/PETS2006/input/'
-
-def generate_bg(videopath):
+def generate_bg(video_path):
     nb_img = 0
     background = None
-    for file in os.listdir(videopath):
+    for file in os.listdir(video_path):
         if file.endswith(".jpg") or file.endswith(".png"):
             nb_img += 1  
-            image = (cv2.imread(os.path.join(videopath, file),cv2.IMREAD_COLOR)).astype(float)
+            image = (cv2.imread(os.path.join(video_path, file),cv2.IMREAD_GRAYSCALE)).astype(float)
             if background is None:
                 background = image
             else:
@@ -21,8 +18,20 @@ def generate_bg(videopath):
         raise Exception('No image in the directory')
     return background/nb_img
 
-bg = generate_bg(videopath)
+def bg_sub(query_path, n=15, background=None, videopath=None):
+    if background is None:
+        background = generate_bg(videopath)
+    image_query = (cv2.imread(query_path,cv2.IMREAD_GRAYSCALE)).astype(float)
+    return np.abs(image_query-background) > n 
 
-plt.imshow(bg, cmap = plt.get_cmap('gray'))
+
+# PATH TO THE THE VIDEO FRAMES
+video_path = 'data/PETS2006/input/'
+# PATH TO THE QUERY PICTURE
+query_path = 'data/PETS2006/input/in000081.jpg'
+
+bg = generate_bg(video_path)
+fg = bg_sub(query_path, background= bg)
+plt.imshow(~fg, cmap = plt.get_cmap('binary'))
 plt.show()
 
